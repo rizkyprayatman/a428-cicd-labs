039e5629f4e1541920f20a0937cd94ed472b29f8
Rizky Prayatman <r.prayatman@gmail.com>
2025-11-29 23:27:02 +0700

    change home view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="https://assets.cdn.dicoding.com/original/commons/certificate_logo.png" className="App-logo" alt="Dicoding Logo" />
+          <h1 className="App-title">
+            <a 
+              href="https://www.dicoding.com/users/rprayatman/academies" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="App-name-link"
+            >
+              Rizky Prayatman
+            </a>
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Click my name to view my Dicoding profile and academies.
         </p>
       </div>
     );
